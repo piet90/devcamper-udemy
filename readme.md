f6bec5e5b5980e89b5e693a04de6213ad99e9646
Peter Gyure <peter.gyure@taager.com>
2023-08-15 11:08:19 +0200

    Initial commit

---
+++ readme.md
@@ -0,0 +1,24 @@
+# DevCamper API
+
+> Backend API for DevCamper application, which is a bootcamp directory website.
+
+## Usage
+
+Rename "config/confog.env.env" to "config/config.env" and update the values/settings to your own.
+
+## Install dependencies
+```
+npm install
+```
+
+## Run app
+```
+# Run in dev mode
+npm run dev
+
+# Run is prod mode
+mpn start
+```
+
+- Version: 1.0.0
+- License: MIT
---
+++ server.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const fileUpload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
+const errorHandler = require('./middleware/error');
+const connectDb = require('./config/db');
+
+dotenv.config({ path: './config/config.env' });
+
+connectDb();
+
+const bootcamps = require('./routes/bootcamps');
+const courses = require('./routes/courses');
+const auth = require('./routes/auth');
+const users = require('./routes/users');
+const reviews = require('./routes/reviews');
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+app.use(fileUpload());
+
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+app.use(xss());
+
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 100,
+});
+app.use(limiter);
+
+app.use(hpp());
+
+app.use(cors());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
+app.use('/api/v1/reviews', reviews);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+    PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+);
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    server.close(() => process.exit(1));
+});
